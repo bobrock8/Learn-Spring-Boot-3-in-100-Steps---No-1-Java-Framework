87008c51de999790068ac550b7042e33b615ec9a
bobrock8 <aleksandarilic990@gmail.com>
2023-02-16 16:32:52 +0100

    - JPA(java persistence api) annotation

--- learn-jpa-and-hibernate/src/main/java/com/aleksandarilic/learnjpaandhibernate/course/Course.java
+++ learn-jpa-and-hibernate/src/main/java/com/aleksandarilic/learnjpaandhibernate/course/Course.java
@@ -1,8 +1,16 @@
 package com.aleksandarilic.learnjpaandhibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name="Course")
 public class Course {
+    @Id
     private long id;
+    @Column(name="name")
     private String name;
+    @Column(name="author")
     private String author;
 
     public Course() {
