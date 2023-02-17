bc038d070acead4caf1959de5e6e752a6d327b45
bobrock8 <aleksandarilic990@gmail.com>
2023-02-17 11:27:14 +0100

    -SecurityFilterChain configuration which enable to open h2 console

--- webapp/src/main/java/com/aleksandarilic/springboot/webapp/security/SpringSecurityConfiguration.java
+++ webapp/src/main/java/com/aleksandarilic/springboot/webapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.aleksandarilic.springboot.webapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -41,5 +44,18 @@ public class SpringSecurityConfiguration {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+          auth -> auth.anyRequest().authenticated()
+        );
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
     }
 }
