ff253f6125ed4a5b05fa41877e050c20a1606898
bobrock8 <aleksandarilic990@gmail.com>
2023-02-17 11:38:14 +0100

    -create todo table with @entity - prepare data for inserting into table

--- webapp/src/main/java/com/aleksandarilic/springboot/webapp/todo/Todo.java
+++ webapp/src/main/java/com/aleksandarilic/springboot/webapp/todo/Todo.java
@@ -1,12 +1,19 @@
 package com.aleksandarilic.springboot.webapp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
     @Size(min=10, message = "Enter at least 10 characters")
     private String description;
---
+++ webapp/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10001, 'bobrock8', 'Get AWS Certified', current_date(), false);
+
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'bobrock8', 'Get JAVA Certified', current_date(), false);
+
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'bobrock8', 'Get React Certified', current_date(), false);
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'bobrock8', 'Get JavaScript Certified', current_date(), false);
