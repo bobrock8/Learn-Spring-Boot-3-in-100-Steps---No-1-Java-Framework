831debba47f3fa657986a1f3b2c452480443a234
bobrock8 <aleksandarilic990@gmail.com>
2023-02-24 11:08:40 +0100

    - Test POST Methods

--- restapi/src/test/java/com/aleksandarilic/restapi/survey/SurveyResourceTests.java
+++ restapi/src/test/java/com/aleksandarilic/restapi/survey/SurveyResourceTests.java
@@ -6,7 +6,11 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Objects;
 
@@ -16,7 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SurveyResourceTests {
 
-	private static String QUESTION_URL = "/surveys/Survey1/questions/Question1";
+	private static String QUESTION_1 = "/surveys/Survey1/questions/Question1";
+	private static String DEFAULT_QUESTION_URL = "/surveys/Survey1/questions";
 
 	@Autowired
 	private TestRestTemplate template;
@@ -37,7 +42,7 @@ class SurveyResourceTests {
 
 	@Test
 	void getSurveyQuestion() throws JSONException {
-		ResponseEntity<String> responseEntity = template.getForEntity(QUESTION_URL, String.class);
+		ResponseEntity<String> responseEntity = template.getForEntity(QUESTION_1, String.class);
 //		[Content-Type:"application/json", Transfer-Encoding:"chunked", Date:"Sun, 19 Feb 2023 17:44:56 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]
 //		{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
 
@@ -46,6 +51,39 @@ class SurveyResourceTests {
 		JSONAssert.assertEquals(str, responseEntity.getBody(), true);
 		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
 		assertEquals("application/json", Objects.requireNonNull(responseEntity.getHeaders().get("Content-Type")).get(0));
+	}
+
+
+	@Test
+	void addNewSurveyQuestion() {
+		// http://localhost:8080/surveys/Survey1/questions/
+		// POST
+		// header Content-Type application/json
+		// 201?
+		// check location header http://localhost:8080/surveys/Survey1/questions/1870694470
+		String body = """
+			   {
+						"description": "Fastest Growing Cloud Platform",
+						"options": [
+							"AWS",
+							"Azure",
+							"Google Cloud",
+							"Oracle Cloud"
+			    		],
+					"correctAnswer": "Google Cloud"
+				}
+			""";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json");
+
+		HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+
+		ResponseEntity<String> responseEntity
+				= template.exchange(DEFAULT_QUESTION_URL, HttpMethod.POST, httpEntity, String.class);
+
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+		assertTrue(responseEntity.getHeaders().get("Location").get(0).contains("/surveys/Survey1/questions/"));
+
 	}
 
 }
